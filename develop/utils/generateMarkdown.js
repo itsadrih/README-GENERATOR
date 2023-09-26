f8c71d5a247061ff3d026f3bade7f7e2fdf9a6e2
Adriana Herrera <adri@Adrianas-MacBook-Air.local>
2023-09-26 18:15:05 -0400

    added new notes and added coded I erased by accident. Finished with markdown file so far.

--- develop/utils/generateMarkdown.js
+++ develop/utils/generateMarkdown.js
@@ -1,4 +1,4 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
+// DONE: CreateD a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
   if (!license) {
@@ -8,8 +8,8 @@ function renderLicenseBadge(license) {
   }
 }
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
+// DONE: Create a function that returns the license link
+
 function renderLicenseLink(license) {
   if (license === 'CC--0') {
     return `http://creativecommons.org/licenses/by-nd/4.0`
@@ -21,3 +21,29 @@ function renderLicenseLink(license) {
  
 }
 
+// DONE - If there is no license, return an empty string
+// DONE: Created a function that returns the license section of README
+
+
+function renderLicenseSection(license) { 
+  if (!license) {
+  return ``;
+} else {
+  return `## Licenses
+  This project is using the ${license} license.`
+}
+
+
+}
+
+// DONE: CreateD a function to generate markdown for README
+function generateMarkdown(data) {
+
+  return `# ${data.title}
+ ${renderLicenseBadge(data.licenses)}
+
+`;
+}
+
+module.exports = generateMarkdown;
+const fs = require('fs');
