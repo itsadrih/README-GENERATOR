c2ec357438520b9b6e92affee7951cb214d8d49d
Adriana Herrera <adri@Adrianas-MacBook-Air.local>
2023-09-26 18:11:14 -0400

    added liscense badge and liscenses.

--- develop/utils/generateMarkdown.js
+++ develop/utils/generateMarkdown.js
@@ -1,21 +1,23 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (!license) {
+    return ``;
+  } else {
+    return `[![${license} license](https://img.shields.io/badge/License-${license}-blue.svg)](${renderLicenseLink(license)})`;
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license === 'CC--0') {
+    return `http://creativecommons.org/licenses/by-nd/4.0`
+  }
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
-
-`;
+  if (license === 'MIT') {
+    return `http://lbesson.mit-license.org/`
+  }
+ 
 }
 
-module.exports = generateMarkdown;
-const fs = require('fs');
